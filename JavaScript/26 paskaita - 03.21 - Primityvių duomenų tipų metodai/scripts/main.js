b314c115ab04f2652f4846706e090d537062cd58
BanRokas <rokas.banaitis@gmail.com>
2023-03-22 08:40:50 +0200

    26 paskaita - Metodai update

--- JavaScript/26 paskaita - 03.21 - Primityvių duomenų tipų metodai/scripts/main.js
+++ JavaScript/26 paskaita - 03.21 - Primityvių duomenų tipų metodai/scripts/main.js
@@ -1,3 +1,14 @@
 console.log('zodis'.at(2));
 console.log('zodis'.charAt(2));
 console.log('zodis'[2]);
+
+// function haha(){
+//   let pirmas = kazkieno.value;
+//   let antras = kazkieno.value;
+
+//   if(!pirmas || !antras){
+//     return 'klaida';
+//   }
+
+//   let ats = pirmas*antras;
+// }

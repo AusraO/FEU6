b714da9995b218cdc26a3d8aa3e2d708b41f95fa
BanRokas <rokas.banaitis@gmail.com>
2023-03-29 15:20:50 +0300

    32 paskaita - Komponentai

--- JavaScript/32 paskaita - 03.29 - Moduliai ir Komponentai/scripts/main.js
+++ JavaScript/32 paskaita - 03.29 - Moduliai ir Komponentai/scripts/main.js
@@ -1,5 +1,44 @@
 import { sum as sudetis, division } from './matematika.js';
 import mumboJumbo, { kita } from './randomColor.js';
+import P from './paragraph.js';
+import H from './heading.js';
+import Card from './card.js';
+
+console.groupCollapsed('moduliai');
 kita();kita();kita();kita();kita();
 console.log(sudetis('spalva:',mumboJumbo()));
 console.log(division(80000, 3));
+console.groupEnd();
+
+const testSection = document.querySelector('#test');
+const paragrafas0 = new P({
+  text:'Sukūrėme paragrafą :)',
+  class: ['raudonasTekstas', 'centruotasTekstas']
+});
+const paragrafas1 = new P({text:'Saulė šviet, varom pabėgiot!!!'});
+const heading0 = new H({
+  text: 'Aš esu antraštė',
+  size: 1,
+  class: ['raudonasTekstas', 'centruotasTekstas', 'didelisTekstas']
+});
+const heading1 = new H({
+  text: 'Aš esu mažiukas antraščiukas',
+  size: 5
+});
+
+const card0 = new Card({
+  class: [],
+  image: {
+    src: 'https://assets2.cbsnewsstatic.com/hub/i/r/2012/02/06/1feb5e95-a644-11e2-a3f0-029118418759/thumbnail/1200x630/64a9aba33675cad48043257194ebc838/breading-cats-q013.jpg',
+    alt: 'cat ale bread'
+  },
+  heading: {
+    text: 'Cat in da bread',
+    size: 1
+  },
+  paragraph: {
+    text: 'Cat is very happy while living inside the bread :)'
+  }
+});
+
+testSection.append(heading0, paragrafas0, paragrafas1, heading1, card0);

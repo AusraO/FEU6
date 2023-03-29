9dd70974105e4185fb7cd8606a658b772d8ad1ca
BanRokas <rokas.banaitis@gmail.com>
2023-03-29 11:39:55 +0300

    32 paskaita - Moduliai

---
+++ JavaScript/32 paskaita - 03.29 - Moduliai ir Komponentai/scripts/main.js
@@ -0,0 +1,5 @@
+import { sum as sudetis, division } from './matematika.js';
+import mumboJumbo, { kita } from './randomColor.js';
+kita();kita();kita();kita();kita();
+console.log(sudetis('spalva:',mumboJumbo()));
+console.log(division(80000, 3));

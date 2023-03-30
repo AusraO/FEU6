65d6cd043b24489ae6bb33f0b1049aeedad1d913
BanRokas <rokas.banaitis@gmail.com>
2023-03-30 10:26:03 +0300

    32 paskaita - task 0.1

--- JavaScript/32 paskaita - 03.29 - Moduliai ir Komponentai/scripts/card.js
+++ JavaScript/32 paskaita - 03.29 - Moduliai ir Komponentai/scripts/card.js
@@ -1,5 +1,6 @@
 import H from './heading.js';
 import P from './paragraph.js';
+import Image from './image.js';
 
 export default class Card{
   constructor(props){
@@ -14,9 +15,7 @@ export default class Card{
       this.cardDiv.classList.add(...this.props.class);
     }
 
-    this.cardImage = document.createElement('img');
-    this.cardImage.setAttribute('src', this.props.image.src);
-    this.props.image.alt && this.cardImage.setAttribute('alt', this.props.image.alt);
+    this.cardImage = new Image(this.props.image);
     this.cardDiv.appendChild(this.cardImage);
 
     this.textDiv = document.createElement('div');
